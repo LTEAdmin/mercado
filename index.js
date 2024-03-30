c104a8d135dc6e087803185621f4ae7f5232874a
Alejandra Flaño <aflanog@latiaencanto.cl>
2024-03-29 21:37:47 -0300

    ruta main y generica para determinar error funciona correctamente

--- index.js
+++ index.js
@@ -2,5 +2,10 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+
+app.listen(port, ()=> console.log(`El servidor se ha levantado en el puerto http://localhost:${port}`))
+
+//Rutas
+app.get('/main', (req, res) => res.send('Mercado')),
 /* Si ruta no existe manda mensaje, ojo esta linea debe estar al final del codigo, sobre todo despues de rutas validas */
-/* app.get(“*”, (req, res) => res.send("para determinar que la ruta no existe") */
+app.get('*', (req, res) => res.send('para determinar que la ruta no existe'))
