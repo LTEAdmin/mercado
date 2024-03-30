735df05931ed16cb01b1fc3b6de84db904d6957f
Alejandra Flaño <aflanog@latiaencanto.cl>
2024-03-29 23:44:21 -0300

    definir Handlebars y rutas de elementos a usar en la app

--- index.js
+++ index.js
@@ -2,10 +2,22 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+// trabajar con handlebars
+const exphbs = require("express-handlebars");
+app.set('view engine', 'hbs');
+app.engine('hbs', exphbs.engine({
+    extname: 'hbs'
+}));
 
-app.listen(port, ()=> console.log(`El servidor se ha levantado en el puerto http://localhost:${PORT}`))
+
+//rutas de elementos a usar en la app
+app.use(express.static('assets'));
+app.use('/bootstrap', express.static('node_modules/bootstrap/dist/css'));
+
+// muestra que puerto se esta usando para el servidor
+app.listen(port, () => console.log(`El servidor se ha levantado en el puerto http://localhost:${PORT}`));
 
 //Rutas
-app.get('/main', (req, res) => res.send('Mercado')),
+app.get('/main', (req, res) => res.send('Mercado'));
 /* Si ruta no existe manda mensaje, ojo esta linea debe estar al final del codigo, sobre todo despues de todas las rutas validas */
 app.get('*', (req, res) => res.send('para determinar que la ruta no existe'))
